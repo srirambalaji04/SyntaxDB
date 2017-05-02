c6758170c4ca61283887236c94569c6793c17f2a
srirambalaji04 <srirambalaji04@gmail.com>
2017-05-02 21:50:50 +0530

    general pull

--- app/src/main/java/com/shiru/syntaxdb/dao/SyntaxDBassetHelper.java
+++ app/src/main/java/com/shiru/syntaxdb/dao/SyntaxDBassetHelper.java
@@ -1,10 +1,8 @@
 package com.shiru.syntaxdb.dao;
 
 import android.content.Context;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
-import android.util.Log;
 
 import com.shiru.syntaxdb.BuildConfig;
 
@@ -15,8 +13,9 @@ public class SyntaxDBassetHelper extends SQLiteOpenHelper {
 
     private static final String TAG = "SDB.SyntaxDBassetHelper";
 
-    public static SyntaxDBassetHelper newInstance(Context context){
-        return new SyntaxDBassetHelper(context, BuildConfig.DATABASE, null, BuildConfig.DB_VERSION);
+    public static SyntaxDBassetHelper newInstance(Context context) {
+        return null;
+//        return new SyntaxDBassetHelper(context, BuildConfig.DATABASE, null, BuildConfig.DB_VERSION);
     }
 
     private SyntaxDBassetHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
@@ -28,28 +27,28 @@ public class SyntaxDBassetHelper extends SQLiteOpenHelper {
         db.rawQuery(createTableConcepts(), null);
     }
 
-    private String createTableConcepts(){
+    private String createTableConcepts() {
         String CREATE_CONCEPTS = "CREATE TABLE " + BuildConfig.TABLE_CONCEPTS + " ("
-        + "id TEXT, "
-        + "name	TEXT, "
-        + "categoryId TEXT, "
-        + "position	INTEGER, "
-        + "languageId TEXT, "
-        + "languageLink TEXT, "
-        + "conceptSearch TEXT, "
-        + "conceptLink TEXT, "
-        + "desc	TEXT, "
-        + "syntax TEXT, "
-        + "notes TEXT, "
-        + "example TEXT, "
-        + "keywords	INTEGER, "
-        + "related TEXT, "
-        + "documentation TEXT, "
-        + "PRIMARY KEY(id, name, categoryId, languageId, languageLink));";
+                + "id TEXT, "
+                + "name	TEXT, "
+                + "categoryId TEXT, "
+                + "position	INTEGER, "
+                + "languageId TEXT, "
+                + "languageLink TEXT, "
+                + "conceptSearch TEXT, "
+                + "conceptLink TEXT, "
+                + "desc	TEXT, "
+                + "syntax TEXT, "
+                + "notes TEXT, "
+                + "example TEXT, "
+                + "keywords	INTEGER, "
+                + "related TEXT, "
+                + "documentation TEXT, "
+                + "PRIMARY KEY(id, name, categoryId, languageId, languageLink));";
         return CREATE_CONCEPTS;
     }
 
-    private String checkTables(){
+    private String checkTables() {
         return "PRAGMA table_info([Concepts])";
     }
 
